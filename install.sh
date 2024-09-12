a90f5de8dc91d21ac961c465e6e02d3c2622fe47
WhaleFall <166845494@qq.com>
2024-09-12 09:38:20 +0800

    modify the TOO buggy install.sh

--- install.sh
+++ install.sh
@@ -23,42 +23,37 @@ copy_file() {
 
 
 if git rev-parse --is-inside-work-tree > /dev/null 2>&1 && git remote -v | grep "$repo" > /dev/null 2>&1; then
-    # if git fetch origin > /dev/null 2>&1; then
-        # git fetch origin
-        if "$(pwd)" != "$dst"; then
-            rm -rf "$dst"
-            mkdir -p "$dst"
+    pwd="$(pwd)"
+    if [ "$pwd" != "$dst" ]; then
+        echo "not in $dst"
+        rm -rf "$dst"
+        mkdir -p "$dst"
 
-            all_files="$(ls -A)"
-            for f in "${all_files[@]}"; do
-                if [ -d "$f" ]; then
-                    cp -r "$f" "$dst"
-                else
-                    cp "$f" "$dst"
-                fi
-            done
-        fi
+        cp -R $pwd/. $dst
 
-        cd "$dst"
-
-        echo "Create symlinks"
-        for file in "${files[@]}"; do
-            if [ ! -L "$HOME/$file" ]; then
-                ln -s "$file" "$HOME/$file"
-                echo "create symlink:"
-                ls -l "$HOME/$file"
+        if [ "$1" == "d" ]; then
+            if [ "$(ls -A $dst)" ]; then
+                cd "$dst"
+                rm -rf "$pwd"
             else
-                echo "symlink $file exists"
+                echo "copy failed"
             fi
-        done
-        echo "Done"
-        
-        # for file in "${files[@]}"; do
-        #     ls -l "$HOME/$file"
-        # done
-    # else
-    #     echo "git fetch error"
-    # fi
+        fi
+    fi
+
+    cd "$dst"
+
+    # echo "Create symlinks"
+    for file in "${files[@]}"; do
+        if [ ! -L "$HOME/$file" ]; then
+            ln -s "$file" "$HOME/$file"
+            echo "create symlink:"
+            ls -l "$HOME/$file"
+        else
+            echo "symlink $file exists"
+        fi
+    done
+    echo "Done"
 else
     echo "install.sh not in the repo"
 fi
